7f768605a1f4eb595427237e0886bb7b84518307
Marko <whoops@iaccidentallyusedarealemailimsorryrandomuser.com>
2025-05-14 13:50:03 +0200

    Show information over the product on product.php

--- dev/product.php
+++ dev/product.php
@@ -1,8 +1,9 @@
 <?php
 include_once("templates/head.inc.php");
-$sql = "SELECT name, price, description, image FROM products WHERE id = ?";
-$statement = $conn->prepare($sql);
-if(!isset($_GET['id'])){
+$product = null;
+if(isset($_GET['id'])){
+	$sql = "SELECT name, price, description, image FROM products WHERE id = ?";
+	$statement = $conn->prepare($sql);
 	$statement->execute([$_GET['id']]);
 	$product = $statement->fetch();
 }
@@ -11,20 +12,20 @@ if(!isset($_GET['id'])){
          <div class="uk-grid">
             <section class="uk-width-1">
                <div class="uk-grid uk-card uk-card-default">
-<? if(!empty($product)): ?>
                   <section class="uk-width-1-2 uk-card-media-left">
-                     <img src="<?= $statement['image'] ?>" class="" alt="" title="" />
+<?php if(!empty($product) && isset($product)): ?>
+                     <img src="<?= 'img/' . $product['image']; ?>" class="" alt="" title="" />
                   </section>
                   <section class="uk-width-1-2 uk-card-body uk-flex uk-flex-column uk-flex-between">
                      <div class="">
-                        <h1><?= $statement['name'] ?></h1>
+                        <h1><?= $product['name']; ?></h1>
                         <p class="">
-<?= $statement['description'] ?>
+<?= $product["description"]; ?>
                         </p>
                      </div>
                      <div class="uk-flex uk-flex-between uk-flex-middle">
                         <div class="price-block">
-                           <p class="product-view__price uk-text-bold uk-text-danger uk-text-left uk-text-bolder">&euro; <?= $statement['price'] ?></p>
+                           <p class="product-view__price uk-text-bold uk-text-danger uk-text-left uk-text-bolder">&euro; <?= $product["price"]; ?></p>
                         </div>
                         <div>
                            <button class="uk-button uk-button-primary">
@@ -33,14 +34,14 @@ if(!isset($_GET['id'])){
                            </button>
                         </div>   
                      </div>
+<?php
+else:
+	echo "<h1><br>Product not found</h1><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"; 
+endif;
+?>
                   </section>
                </div>
             </section>
-<?
-else:
-	echo "<h1>Product not found</h1>"; 
-endif;
-?>
          </div>
       </main>
 <?php
