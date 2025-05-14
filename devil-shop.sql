17f8f0102d093055658f3982c9586e7da464020c
sleepyniki <nickmiddel2000685@gmail.com>
2025-05-14 13:16:59 +0200

    removed of all chickens (one been spared cuz of marco) and updated the sql file

--- devil-shop.sql
+++ devil-shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2025 at 12:28 PM
+-- Generation Time: May 14, 2025 at 01:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -37,19 +37,19 @@ CREATE TABLE `cart-items` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `catagory`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `catagory` (
-  `categorie_id` int(11) NOT NULL,
-  `categorie_name` varchar(100) NOT NULL
+CREATE TABLE `category` (
+  `categories_id` int(11) NOT NULL,
+  `categories_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `catagory`
+-- Dumping data for table `category`
 --
 
-INSERT INTO `catagory` (`categorie_id`, `categorie_name`) VALUES
+INSERT INTO `category` (`categories_id`, `categories_name`) VALUES
 (1, 'Demons'),
 (2, 'Contracts'),
 (3, 'Summoning materials'),
@@ -68,23 +68,23 @@ CREATE TABLE `products` (
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `categorie` varchar(255) NOT NULL
+  `categories` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `price`, `name`, `description`, `image`, `categorie`) VALUES
-(1, 150.00, 'phone call with satan', 'when buying this product you will be given a phone call with the dark lord him self satan! this call i guaranteed to last at least 666 second\'s ', '', 'Demons'),
-(2, 50.00, 'ticked to hell  ', 'this is a ticked to hell when you say that someone should go to hell this is the gift for them buying this item will send the person straight to hell', '', 'Summoning materials '),
-(3, 20.00, 'Summoning my first demon, a children\'s book, for young children', 'what a good way for children to learn how to summon a demon? in this book we will explain how a child can summon there first demon ', '', 'books'),
-(4, 20.00, ' I accidentally summoned a demon and I don\'t know what to do, een love romance novel', 'in this book we follow the main protagonist\r\nwho accidentally summons a demon and falls in love with them a trilling story about love danger and demonic drama', '', 'books'),
-(5, 10.00, 'summoning Candles', 'this is a set of summoning Candles ', '', 'Summoning materials'),
-(6, 70.00, 'Artificial blood ', 'this is a Artificial blood pack for your summoning need ', '', 'Summoning materials'),
-(7, 66.66, 'Satan\'s contracts booster pack', 'this is a booster pack for your soul contracting needs ', '', 'Collectibles'),
-(8, 18.99, ' Dante’s Inferno, a travel guide ', 'Dante\'s Inferno. An epic and searing poem, that takes the reader on an intense journey through the darkest pits of hell. As as travel guide', '', 'books'),
-(9, 300.00, '3000 year old paper', 'this is a paper from 3000 years ago use it for all your contracting need ', '', 'Contracts');
+INSERT INTO `products` (`id`, `price`, `name`, `description`, `image`, `categories`) VALUES
+(1, 150.00, 'phone call with satan', 'when buying this product you will be given a phone call with the dark lord him self satan! this call i guaranteed to last at least 666 second\'s ', 'phone.png', 'Demons'),
+(2, 50.00, 'ticket to hell  ', 'this is a ticket to hell when you say that someone should go to hell this is the gift for them buying this item will send the person straight to hell', 'ticket.png', 'Summoning materials '),
+(3, 20.00, 'Summoning my first demon, a children\'s book, for young children', 'what a good way for children to learn how to summon a demon? in this book we will explain how a child can summon there first demon ', 'summoning.png', 'books'),
+(4, 20.00, ' I accidentally summoned a demon and I don\'t know what to do, een love romance novel', 'in this book we follow the main protagonist\r\nwho accidentally summons a demon and falls in love with them a trilling story about love danger and demonic drama', 'book.png', 'books'),
+(5, 10.00, 'summoning Candles', 'this is a set of summoning Candles ', 'candle.png', 'Summoning materials'),
+(6, 70.00, 'Artificial blood ', 'this is a Artificial blood pack for your summoning need ', 'bloodbag.png', 'Summoning materials'),
+(7, 66.66, 'Satan\'s contracts booster pack', 'this is a booster pack for your soul contracting needs ', 'satans-contracts-boosterpack-1.png', 'Collectibles'),
+(8, 18.99, ' Dante’s Inferno, a travel guide ', 'Dante\'s Inferno. An epic and searing poem, that takes the reader on an intense journey through the darkest pits of hell. As as travel guide', 'inferno.png', 'books'),
+(9, 300.00, '3000 year old paper', 'this is a paper from 3000 years ago use it for all your contracting need ', 'paper.png', 'Contracts');
 
 -- --------------------------------------------------------
 
@@ -127,10 +127,10 @@ ALTER TABLE `cart-items`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `catagory`
+-- Indexes for table `category`
 --
-ALTER TABLE `catagory`
-  ADD PRIMARY KEY (`categorie_id`);
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categories_id`);
 
 --
 -- Indexes for table `products`
@@ -161,10 +161,10 @@ ALTER TABLE `cart-items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `catagory`
+-- AUTO_INCREMENT for table `category`
 --
-ALTER TABLE `catagory`
-  MODIFY `categorie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `category`
+  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -182,7 +182,7 @@ ALTER TABLE `shopping-cart`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
