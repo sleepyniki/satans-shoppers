64aa8d79c57d2a02daafbd6f297a74bee53d0033
sleepyniki <nickmiddel2000685@gmail.com>
2025-05-20 12:30:09 +0200

    .

--- dev/order_confirm.php
+++ dev/order_confirm.php
@@ -24,53 +24,6 @@ require_once("templates/head.inc.php");
                </div>
             </section>
             <!-- EINDE: BEDANKT -->
-
-            <!-- BEGIN: EINDAFREKENING -->
-            <section class="uk-width-3-3">
-               <div class="uk-card-default uk-card-small uk-flex uk-flex uk-flex-column uk-flex-between uk-padding-small">
-                  <div class="uk-card-header">
-                     <h2>Your order contains</h2>
-                  </div>
-                  <div class="uk-card-body uk-flex uk-flex-column uk-flex-between">
-                     <table class="uk-table uk-table-divider uk-width-2-2 order-confirm-table">
-                        <thead>
-                           <tr>
-                                 <th class="uk-width-2-3">Product</th>
-                                 <th class="uk-text-center">Price</th>
-                                 <th class="uk-text-center">Quantity</th>
-                                 <th class="uk-text-right">In Total</th>
-                           </tr>
-                        </thead>
-                        <tbody>
-                           <tr>
-                                 <td class="uk-flex uk-flex-middle uk-gap">
-                                    <img class="uk-order-confirm-img" src="" alt="" />
-                                    <p class="uk-margin-remove">Name</p>
-                                 </td>
-                                 <td class="uk-text-center">&euro; 0.00</td>
-                                 <td class="uk-text-center">0</td>
-                                 <td class="uk-text-right">&euro; 0.00</td>
-                           </tr>
-                        </tbody>
-                        <tfoot>
-                           <tr>
-                              <td colspan="3" class="uk-text-right uk-text-uppercase">In Total</td>
-                              <td class="uk-text-right">&euro; 0.00</td>
-                           </tr>
-                           <tr>
-                              <td colspan="3" class="uk-text-right uk-text-uppercase">Already paid</td>
-                              <td class="uk-text-right">&euro; 0.00</td>
-                           </tr>
-                           <tr>
-                              <td colspan="3" class="uk-text-right uk-text-uppercase uk-text-bolder">Still needed to be payed</td>
-                              <td class="uk-text-right uk-text-bolder">&euro; 0.00</td>
-                           </tr>
-                        </tfoot>
-                     </table>
-                  </div>
-               </div>
-            </section>
-            <!-- EINDE: EINDAFREKENING -->
          </div>
       </main>
 <?php
